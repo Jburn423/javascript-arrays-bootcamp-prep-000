c979538a9588300de7eab1997cfa4c3f1f26e742
Jonah Burstein <jburstei@u.rochester.edu>
2018-01-22 21:45:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,7 +39,7 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
 }destructivelyRemoveElementFromBeginningOfArray(chocolateBars);
 
 function removeElementFromBeginningOfArray(chocolateBars){
-  // chocolateBars.slice(1);
-  // return chocolateBars;
-  return[chocolateBars.slice(1)];
+  chocolateBars.slice(1);
+  return chocolateBars;
+  // return[chocolateBars.slice(1)];
 }removeElementFromBeginningOfArray(chocolateBars);
